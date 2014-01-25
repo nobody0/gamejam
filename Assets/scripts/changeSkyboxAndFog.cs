0989add57e453118f8725611790c957ae69d3bc5
raphael <student@sae.sae>
2014-01-25 21:15:24 +0100

    fog and skybox changer

---
+++ Assets/scripts/changeSkyboxAndFog.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeSkyboxAndFog : MonoBehaviour {
+
+    public Material skyboxSummer;
+    public Material skyboxWinter;
+
+    public Color fogColorSummer;
+    public float fogDensitySummer;
+    public float fogEndDistanceSummer;
+    public float fogStartDistanceSummer;
+    public FogMode fogModeSummer;
+
+    public Color fogColorWinter;
+    public float fogDensityWinter;
+    public float fogEndDistanceWinter;
+    public float fogStartDistanceWinter;
+    public FogMode fogModeWinter;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        if (GameModel.PlayerId == GameModel.Characters.Summer)
+        {
+            RenderSettings.skybox = skyboxSummer;
+
+            RenderSettings.fog = true;
+            RenderSettings.fogColor = fogColorSummer;
+            RenderSettings.fogDensity = fogDensitySummer;
+            RenderSettings.fogEndDistance = fogEndDistanceSummer;
+            RenderSettings.fogStartDistance = fogStartDistanceSummer;
+            RenderSettings.fogMode = fogModeSummer;
+
+            enabled = false;
+        }
+
+        if (GameModel.PlayerId == GameModel.Characters.Summer)
+        {
+            RenderSettings.skybox = skyboxWinter;
+
+            RenderSettings.fog = true;
+            RenderSettings.fogColor = fogColorWinter;
+            RenderSettings.fogDensity = fogDensityWinter;
+            RenderSettings.fogEndDistance = fogEndDistanceWinter;
+            RenderSettings.fogStartDistance = fogStartDistanceWinter;
+            RenderSettings.fogMode = fogModeWinter;
+
+            enabled = false;
+        }
+	}
+}
