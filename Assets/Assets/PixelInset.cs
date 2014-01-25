a307930300f81dfe69e3f270032f306e4ec97cf3
steven <student@sae.de>
2014-01-25 20:31:10 +0100

    guigit add Assets/Assets/GGJ_*

---
+++ Assets/Assets/PixelInset.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PixelInset : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Rect rect = guiTexture.pixelInset;
+		rect.height = Screen.height;
+		rect.width = Screen.width;
+		guiTexture.pixelInset = rect;
+	}
+}
